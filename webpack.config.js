8f89f544a3b515f7b2cf64b1bafa2d2da5a01c4b
TristanJM <t.j.murfitt-14@student.lboro.ac.uk>
2017-01-29 17:28:47 +0000

    Added minified bundle

--- webpack.config.js
+++ webpack.config.js
@@ -2,31 +2,37 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-    entry: './scripts/script.js',
-//		entry: './es6/main.js',
-    output: {
-        path: __dirname,
-        filename: 'bundle.js'
-    },
-    module: {
-        loaders: [
-            {
-                loader: 'babel-loader',
-                test: path.join(__dirname, 'scripts'),
-                query: {
-                  presets: 'es2015',
-                },
-            }
-        ]
-    },
-    plugins: [
-        // Avoid publishing files when compilation fails
-        new webpack.NoErrorsPlugin()
-    ],
-    stats: {
-        // Nice colored output
-        colors: true
-    },
-    // Create Sourcemaps for the bundle
-    devtool: 'source-map',
+  entry: {
+		'bundle': './scripts/script.js',
+		'bundle.min': './scripts/script.js',
+	},
+  output: {
+    path: 'dist',
+			filename: '[name].js'
+	},
+	module: {
+		loaders: [
+			{
+				loader: 'babel-loader',
+				test: path.join(__dirname, 'scripts'),
+				query: {
+					presets: 'es2015',
+				},
+			}
+		]
+	},
+	plugins: [
+		// Avoid publishing files when compilation fails
+		new webpack.NoErrorsPlugin(),
+		new webpack.optimize.UglifyJsPlugin({
+			include: /\.min\.js$/,
+			minimize: true
+		})
+	],
+	stats: {
+		// Nice coloured output
+		colors: true
+	},
+	// Create Sourcemaps for the bundle
+	devtool: 'source-map',
 };
